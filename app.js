ce01b0b698d137327253cd63eb5ece16ed929db8
Gideon Weiler <gideon.a.weiler@gmail.com>
2018-03-23 17:10:15 -0400

    v2 of ECT website. Includes new script source to work OOO

--- app.js
+++ app.js
@@ -3,29 +3,46 @@ var app = angular.module('app', []);
 app.controller('Controller', ['$scope', '$window', '$location', '$anchorScroll',
   function($scope, $window, $location, $anchorScroll) {
     $scope.demos = [{
-      name: "Header Map",
-      url: "header-map.html"
-    },{
-      name: "Big Map",
-      url: "big-map.html"
-    }, 
+      name: "Tile View",
+      url: "tile-view.html"
+    },
     {
       name: "List View",
       url: "list-view.html"
     }, 
-
     {
-      name: "List View Images ",
+      name: "Header Map",
+      url: "header-map.html"
+    },
+    {
+      name: "Big Map",
+      url: "big-map.html"
+    }, 
+    {
+      name: "List with Images ",
       url: "list-view-images2.html"
     }, 
     {
-      name: "List View Images 2",
+      name: "List with Images 2",
       url: "list-view-images.html"
+    },
+
+    {
+      name: "Capital One Example", 
+      url: "Cap1.html"
+
+    }, 
+
+    {
+      name: "Ben & Jerrys Example", 
+      url: "BJ_Example.html"
+
     }, 
     {
-      name: "List View Background", 
-      url: "list-view-bg.html"
+      name: "Core Power - Big Map", 
+      url: "CorePower_example.html"
     }
+
     ]
   }
 ])
